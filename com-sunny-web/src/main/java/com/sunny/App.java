97d8e338409b2993a7d73627832ae3255994498b
dongyangz <dongyangz@tujia.com>
2020-03-23 17:31:35 +0800

    pom依赖修复

--- com-sunny-web/src/main/java/com/sunny/App.java
+++ com-sunny-web/src/main/java/com/sunny/App.java
@@ -9,11 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -25,15 +23,14 @@ import javax.sql.DataSource;
 
 /**
  * springboot 项目启动类
+ *
  * @author zhao.dy
  * @date 2018/4/16
  */
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.sunny"})
 @EnableTransactionManagement
 @EnableScheduling
 @ImportResource({"classpath:spring-dubbo-*.xml"})
-@ComponentScan("com.sunny")
 @MapperScan("com.sunny.dao")
 public class App {
 
@@ -43,7 +40,7 @@ public class App {
     private String zkConnectString;
 
     @Bean
-    @ConfigurationProperties(prefix="spring.datasource")
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return new com.alibaba.druid.pool.DruidDataSource();
     }
@@ -65,8 +62,8 @@ public class App {
         return new DataSourceTransactionManager(dataSource());
     }
 
-    public static void main(String[] args){
-        SpringApplication.run(App.class,args);
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
         logger.info("测试静态加载={}", JSON.toJSONString(ConstantController.map));
         logger.info("sunny project start success!");
     }
