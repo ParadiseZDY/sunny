7c7fc321ac64756d5f5b5b271c13593c739f37c5
zhaodongyangbj <zhaodongyangbj@yuanfudao.com>
2022-08-25 10:08:48 +0800

    格式化

--- com-sunny-service/src/main/java/com/sunny/service/GmsConfigServiceImpl.java
+++ com-sunny-service/src/main/java/com/sunny/service/GmsConfigServiceImpl.java
@@ -5,64 +5,62 @@ import com.sunny.domain.GmsConfig;
 import com.sunny.rpc.IGmsConfigService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 
 /**
- * 
- * 
  * @author zhao.dy
  * @date 2018-04-16 12:20
  **/
 @Service("gmsConfigService")
 public class GmsConfigServiceImpl implements IGmsConfigService {
 
-	private static Logger logger = LoggerFactory.getLogger(GmsConfigServiceImpl.class);
+    private static Logger logger = LoggerFactory.getLogger(GmsConfigServiceImpl.class);
 
-	@Resource
-	private GmsConfigDao gmsConfigDao;
+    @Autowired
+    private GmsConfigDao gmsConfigDao;
 
-	@Override
-	public GmsConfig selectByPrimaryKey(Integer id) {
-		return gmsConfigDao.selectByPrimaryKey(id);
-	}
+    @Override
+    public GmsConfig selectByPrimaryKey(Integer id) {
+        return gmsConfigDao.selectByPrimaryKey(id);
+    }
 
-	@Override
-	public int deleteByPrimaryKey(Integer id) {
-		return gmsConfigDao.deleteByPrimaryKey(id);
-	}
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return gmsConfigDao.deleteByPrimaryKey(id);
+    }
 
-	@Override
-	public int insertSelective(GmsConfig gmsConfig) {
-		return gmsConfigDao.insertSelective(gmsConfig);
-	}
+    @Override
+    public int insertSelective(GmsConfig gmsConfig) {
+        return gmsConfigDao.insertSelective(gmsConfig);
+    }
 
-	@Override
-	public int updateByPrimaryKeySelective(GmsConfig gmsConfig) {
-		return gmsConfigDao.updateByPrimaryKeySelective(gmsConfig);
-	}
+    @Override
+    public int updateByPrimaryKeySelective(GmsConfig gmsConfig) {
+        return gmsConfigDao.updateByPrimaryKeySelective(gmsConfig);
+    }
 
-	@Override
-	public int updateByPrimaryKey(GmsConfig gmsConfig) {
-		return gmsConfigDao.updateByPrimaryKey(gmsConfig);
-	}
+    @Override
+    public int updateByPrimaryKey(GmsConfig gmsConfig) {
+        return gmsConfigDao.updateByPrimaryKey(gmsConfig);
+    }
 
-	@Override
-	public Long selectObjectListPageTotal(GmsConfig gmsConfig) {
-		return gmsConfigDao.selectObjectListPageTotal(gmsConfig);
-	}
+    @Override
+    public Long selectObjectListPageTotal(GmsConfig gmsConfig) {
+        return gmsConfigDao.selectObjectListPageTotal(gmsConfig);
+    }
 
-	@Override
-	public List<GmsConfig> selectObjectListPage(GmsConfig gmsConfig) {
-		return gmsConfigDao.selectObjectListPage(gmsConfig);
-	}
+    @Override
+    public List<GmsConfig> selectObjectListPage(GmsConfig gmsConfig) {
+        return gmsConfigDao.selectObjectListPage(gmsConfig);
+    }
 
-	@Override
-	public List<GmsConfig> selectByObjectList(GmsConfig gmsConfig){
-		return gmsConfigDao.selectByObjectList(gmsConfig);
-	}
+    @Override
+    public List<GmsConfig> selectByObjectList(GmsConfig gmsConfig) {
+        return gmsConfigDao.selectByObjectList(gmsConfig);
+    }
 
 }
